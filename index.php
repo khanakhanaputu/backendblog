ebc50d093cca3f697c6179811ca43678f1c3e801
ArtaHendraa <artahenda@gmail.com>
2025-04-28 09:39:22 +0800

    route check for api and regular page handling

--- index.php
+++ index.php
@@ -6,18 +6,27 @@ $Router = new Router();
 
 // Routing auto bos wkwkwkwkwk
 
+// ngecek url ada /api atau ngga
+if (isset($_SERVER['REQUEST_URI']) && strpos($_SERVER['REQUEST_URI'], '/api') === 0) {
+    // kalo ada /api tampiannya cuman jsone response
+    header('Content-Type: application/json; charset=UTF-8');
+    $Router->run();  // ngejalanin router api
+} else {
+    // kalo nggaada /api nampilih struktur html untuk view nya
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/public/css/output.css">
+    </head>
+    <body class="font-poppins">
+        <?php
+        $Router->run();
+        ?>
+    </body>
+    </html>
+    <?php
+}
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/public/css/output.css">
-
-</head>
-<body class="font-poppins">
-  <?php
-    $Router->run();
-  ?>
-</body>
-</html>
